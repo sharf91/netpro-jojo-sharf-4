df17afb384a9da3e30e8c42e91917034ded44633
Sharif Sharif <sharifs@kth.se>
2018-12-06 12:03:09 +0100

    stash

---
+++ src/main/java/domain/ConversionRate.java
@@ -0,0 +1,47 @@
+package domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.PositiveOrZero;
+
+@Entity
+public class ConversionRate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", updatable = false, nullable = false)
+    private Long id;
+    private String fromCurr;
+    private String toCurr;
+    private double rate;
+
+    protected ConversionRate() {
+        // Required by JPA
+    }
+
+    public ConversionRate(String fromCurr, String toCurr) {
+        this.fromCurr = fromCurr;
+        this.toCurr = toCurr;
+    }
+
+    public Long getId() {
+        return id; }
+
+    public String getFromCurr() {
+        return fromCurr;
+    }
+
+    public String getToCurr() {
+        return toCurr;
+    }
+
+    @PositiveOrZero
+    public double getRate() {
+        return rate;
+    }
+
+    public String toString() {
+        return String.format(
+                "ConversionRate[id=%d, fromCurr='%s', toCurr='%s', rate='%d']",
+                id, fromCurr, toCurr, rate);
+    }
+}
